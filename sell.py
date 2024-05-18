6b8dd7cbc31fd10849c89c718eb97e92dd9eb1af
mauludani <63839982+mauludani@users.noreply.github.com>
2024-05-18 15:54:10 +0700

    update

--- sell.py
+++ sell.py
@@ -11,40 +11,41 @@ def capture_screen_area(left, top, width, height):
 def main(index):
     # Contoh pemanggilan fungsi capture_screen_area dengan koordinat (left, top) dan ukuran (width, height) yang diinginkan
     left = 0
-    top = 134
-    width = 190
-    height = 450
+    top = 100
+    width = 76
+    height = 800
 
     screenshot = capture_screen_area(left, top, width, height)
-    # screenshot.show()  # Tampilkan tangkapan layar dalam jendela pop-up
+    screenshot.show()  # Tampilkan tangkapan layar dalam jendela pop-up
 
     # Convert screenshot to bytes
-    buffer = BytesIO()
-    if screenshot.mode != 'RGB':
-        screenshot = screenshot.convert('RGB')
+    # buffer = BytesIO()
+    # if screenshot.mode != 'RGB':
+    #     screenshot = screenshot.convert('RGB')
 
-    screenshot = ImageOps.invert(screenshot)
-    screenshot.save(buffer, format="PNG")
-    buffer.seek(0)
+    # screenshot = ImageOps.invert(screenshot)
+    # screenshot.save(buffer, format="PNG")
+    # buffer.seek(0)
 
-    # Define your backend URL to upload the screenshot
-    backend_url = "http://127.0.0.1:8000/api/upload"
+    # # Define your backend URL to upload the screenshot
+    # backend_url = "http://127.0.0.1:8000/api/upload"
 
-    # Set up the files parameter with the screenshot
-    files = {'file': buffer}
+    # # Set up the files parameter with the screenshot
+    # files = {'file': buffer}
 
-    # Make a POST request to upload the screenshot
-    response = requests.post(backend_url, files=files)
+    # # Make a POST request to upload the screenshot
+    # response = requests.post(backend_url, files=files)
 
-    # Check the response
-    if response.status_code == 200:
-        print(index)
-    else:
-        print("Failed to upload screenshot. Status code:", response.status_code)
+    # # Check the response
+    # if response.status_code == 200:
+    #     print(index)
+    # else:
+    #     print("Failed to upload screenshot. Status code:", response.status_code)
 
 if __name__ == "__main__":
     index = 0
-    while True:
-        index = index + 1
-        main(index)
-        time.sleep(5)  # Sleep for 1 second
+    main(index)
+    # while True:
+    #     index = index + 1
+    #     main(index)
+    #     time.sleep(5)  
--- test.py
+++ test.py
@@ -1,8 +1,64 @@
-from PIL import Image
-import pytesseract
+import pyautogui
+import requests
+import time
+import sys
+from pytesseract import pytesseract
+from PIL import Image, ImageOps
+from io import BytesIO
 
-filename = 'buy3.png'
-img1 = Image.open(filename)
-text = pytesseract.image_to_string(img1)
+def capture_screen_area(left, top, width, height):
+    screenshot = pyautogui.screenshot(region=(left, top, width, height))
+    return screenshot
 
-print(text)
+def main(index, coin: str):
+    # Contoh pemanggilan fungsi capture_screen_area dengan koordinat (left, top) dan ukuran (width, height) yang diinginkan
+    left = 0
+    top = 100
+    width = 76
+    height = 800
+
+    screenshot = capture_screen_area(left, top, width, height)
+    # screenshot.show()  # Tampilkan tangkapan layar dalam jendela pop-up
+
+    # Convert screenshot to bytes
+    buffer = BytesIO()
+    if screenshot.mode != 'RGB':
+        screenshot = screenshot.convert('RGB')
+
+    # screenshot = ImageOps.invert(screenshot)
+    screenshot.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    # Define your backend URL to upload the screenshot
+    backend_url = "http://127.0.0.1:8000/api/upload"
+
+    # Set up the files parameter with the screenshot
+    files = {'file': buffer}
+    textt = pytesseract.image_to_string(screenshot)
+
+    data = {
+        'text': textt,
+        'coin': coin,
+    }
+    # print(text)
+    # Make a POST request to upload the screenshot
+    response = requests.post(backend_url, data=data)
+
+    print('--start--')
+    print(textt)
+    # Check the response
+    if response.status_code == 200:
+        print(index)
+        print(coin)
+    else:
+        print("Error : ", response.content)
+
+
+    print('--end--')
+    print(' ')
+if __name__ == "__main__":
+    index = 0
+    while True:
+        index = index + 1
+        main(index, coin= sys.argv[1])
+        time.sleep(2)
